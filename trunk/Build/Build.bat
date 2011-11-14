067572dca98537cac2c111f2ab1c15eb98361a9b
yoavain <yoavain@4f3f5a9e-659f-0abd-27a3-c6013623afec>
2011-11-14 14:55:43 +0000

    Fix build scripts

--- trunk/Build/Build.bat
+++ trunk/Build/Build.bat
@@ -5,6 +5,8 @@ cd ..
 
 cd SubsCenterOrg
 "%WINDIR%\Microsoft.NET\Framework\v3.5\MSBUILD.exe" /target:Rebuild /property:Configuration=RELEASE SubsCenterOrg.csproj
+cd ..
+cd Sratim
 "%WINDIR%\Microsoft.NET\Framework\v3.5\MSBUILD.exe" /target:Rebuild /property:Configuration=RELEASE Sratim.csproj
 cd ..
 
--- trunk/Build/Build_Debug.bat
+++ trunk/Build/Build_Debug.bat
@@ -5,6 +5,8 @@ cd ..
 
 cd SubsCenterOrg
 "%WINDIR%\Microsoft.NET\Framework\v3.5\MSBUILD.exe" /target:Rebuild /property:Configuration=DEBUG SubsCenterOrg.csproj
+cd ..
+cd Sratim
 "%WINDIR%\Microsoft.NET\Framework\v3.5\MSBUILD.exe" /target:Rebuild /property:Configuration=DEBUG Sratim.csproj
 cd ..
 
