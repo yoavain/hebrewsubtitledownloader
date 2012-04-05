99b4039a26a26b01b993ebeab559df4c05f70966
yoavain <yoavain@4f3f5a9e-659f-0abd-27a3-c6013623afec>
2012-04-05 05:48:25 +0000

    Getting ready for 2.2.x

--- SubsCenterOrg/Properties/AssemblyInfo.cs
+++ SubsCenterOrg/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SubsCenterOrg")]
-[assembly: AssemblyCopyright("Copyright © 2011")]
+[assembly: AssemblyCopyright("Copyright © 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.0.$WCREV$")]
-[assembly: AssemblyFileVersion("2.1.0.$WCREV$")]
+[assembly: AssemblyVersion("2.2.0.$WCREV$")]
+[assembly: AssemblyFileVersion("2.2.0.$WCREV$")]
--- SubsCenterOrgTest/Properties/AssemblyInfo.cs
+++ SubsCenterOrgTest/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SubsCenterOrgTest")]
-[assembly: AssemblyCopyright("Copyright © 2011")]
+[assembly: AssemblyCopyright("Copyright © 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.1.0.$WCREV$")]
-[assembly: AssemblyFileVersion("2.1.0.$WCREV$")]
+[assembly: AssemblyVersion("2.2.0.$WCREV$")]
+[assembly: AssemblyFileVersion("2.2.0.$WCREV$")]
 
