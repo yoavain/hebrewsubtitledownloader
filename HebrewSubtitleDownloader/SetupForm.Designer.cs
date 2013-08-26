689ac4e1eb2ce54c6c7a91c5fb4dff7653b5d557
yoavain <yoavain@4f3f5a9e-659f-0abd-27a3-c6013623afec>
2013-08-26 20:01:09 +0000

    GUI changes

--- HebrewSubtitleDownloader/SetupForm.Designer.cs
+++ HebrewSubtitleDownloader/SetupForm.Designer.cs
@@ -31,13 +31,17 @@
             this.updateLoginInfo = new System.Windows.Forms.Button();
             this.status = new System.Windows.Forms.Label();
             this.exitButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // updateLoginInfo
             // 
-            this.updateLoginInfo.Location = new System.Drawing.Point(114, 74);
+            this.updateLoginInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.updateLoginInfo.Location = new System.Drawing.Point(83, 76);
             this.updateLoginInfo.Name = "updateLoginInfo";
-            this.updateLoginInfo.Size = new System.Drawing.Size(111, 38);
+            this.updateLoginInfo.Size = new System.Drawing.Size(143, 38);
             this.updateLoginInfo.TabIndex = 0;
             this.updateLoginInfo.Text = "Update Login info";
             this.updateLoginInfo.UseVisualStyleBackColor = true;
@@ -46,34 +50,55 @@
             // status
             // 
             this.status.AutoSize = true;
-            this.status.Location = new System.Drawing.Point(111, 9);
+            this.status.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.status.Location = new System.Drawing.Point(80, 19);
             this.status.Name = "status";
-            this.status.Size = new System.Drawing.Size(117, 13);
+            this.status.Size = new System.Drawing.Size(157, 17);
             this.status.TabIndex = 1;
             this.status.Text = "Sratim.co.il login status:";
             // 
             // exitButton
             // 
-            this.exitButton.Location = new System.Drawing.Point(114, 163);
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.exitButton.Location = new System.Drawing.Point(83, 152);
             this.exitButton.Name = "exitButton";
-            this.exitButton.Size = new System.Drawing.Size(111, 38);
+            this.exitButton.Size = new System.Drawing.Size(143, 38);
             this.exitButton.TabIndex = 2;
             this.exitButton.Text = "Done";
             this.exitButton.UseVisualStyleBackColor = true;
             this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.statusLabel);
+            this.panel1.Controls.Add(this.status);
+            this.panel1.Controls.Add(this.exitButton);
+            this.panel1.Controls.Add(this.updateLoginInfo);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(319, 237);
+            this.panel1.TabIndex = 3;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.statusLabel.Location = new System.Drawing.Point(123, 47);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(0, 17);
+            this.statusLabel.TabIndex = 3;
             // 
             // SetupForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(344, 262);
-            this.Controls.Add(this.exitButton);
-            this.Controls.Add(this.status);
-            this.Controls.Add(this.updateLoginInfo);
+            this.Controls.Add(this.panel1);
             this.Name = "SetupForm";
             this.Text = "SetupForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -82,5 +107,7 @@
         private System.Windows.Forms.Button updateLoginInfo;
         private System.Windows.Forms.Label status;
         private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label statusLabel;
     }
 }
