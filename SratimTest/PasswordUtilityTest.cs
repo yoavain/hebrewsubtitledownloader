4350ff817a70e34c55f5e492fa55f2145b5d561e
yoavain <yoavain@4f3f5a9e-659f-0abd-27a3-c6013623afec>
2013-08-26 19:49:03 +0000

    Change PasswordUtility to my needs and added a test

---
+++ SratimTest/PasswordUtilityTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web.Security;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sratim;
+
+namespace SratimTest
+{
+    [TestClass]
+    public class PasswordUtilityTest
+    {
+        [TestMethod]
+        public void TestRandomEncryptDecrypt()
+        {
+            // Generate password
+            var generatedPassword = Membership.GeneratePassword(12, 0);
+            var passwordBytes = Encoding.ASCII.GetBytes(generatedPassword);
+
+            // Encript
+            var encryptedData = PasswordUtility.EncryptData(passwordBytes, DataProtectionScope.LocalMachine);
+
+            // Decript
+            var decryptedData = PasswordUtility.DecryptData(encryptedData, DataProtectionScope.LocalMachine);
+
+            Assert.IsTrue(passwordBytes.SequenceEqual(decryptedData));
+        }
+    }
+}
